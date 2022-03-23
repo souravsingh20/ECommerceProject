72c82077c1fb66501323bb3b0d779f21389bc12b
Sourav Singh <68164660+souravsingh20@users.noreply.github.com>
2022-03-23 10:45:15 +0530

    Delete seller_microservice directory

--- seller_microservice/src/main/java/com/quinbay/ecommerce/seller_microservice/service/SellerService.java
+++
@@ -1,7 +0,0 @@
-package com.quinbay.ecommerce.seller_microservice.service;
-
-import com.quinbay.ecommerce.seller_microservice.dto.Seller;
-
-public interface SellerService {
-    void addSeller(Seller seller);
-}
